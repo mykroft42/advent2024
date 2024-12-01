3a6b63b2348abc19566c5dde27e14351d91a3f72
matt.kemmerer <mykroft@gmail.com>
2024-12-01 13:41:55 -0500

    Completed both parts of day 1

---
+++ day01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+public static class Program
+{
+    public static void Main(string[] ags)
+    {
+        var input = File.ReadAllLines("input.txt");
+
+        var list1 = new List<int>();
+        var list2 = new List<int>();
+        foreach(var i in input)
+        {
+            var things = i.Split("   ");
+            
+            list1.Add(int.Parse(things[0]));
+            list2.Add(int.Parse(things[1]));
+        }
+        list1.Sort();
+        list2.Sort();
+        int sum = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            sum += Math.Abs(list1[i] - list2[i]);
+        }
+
+        Console.WriteLine(sum);
+
+        int simil = 0;
+        for (int i = 0; i < list1.Count; i++)
+        {
+            int item = list1[i];
+            simil += item * list2.Where(x => x == item).Count();
+        }
+
+        Console.WriteLine(simil);
+    }
+}
